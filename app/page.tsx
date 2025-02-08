19a56131f085c3ba522e378374df366d9edfec24
Trevor Bedson <trevobedson@gmail.com>
2025-02-08 14:01:26 -0500

    side nav, icons

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,69 @@ import {WeatherStation} from "@/app/api/route";
 
 export default function Home() {
 
+	const [selectedNode, setSelectedNode] = useState<WeatherStation | null>(null);
 	const [data, setData] = useState<WeatherStation[]>([]);
 
 	useEffect(() => {
 		setTimeout(() => {
-			fetch("/api").then((res) => res.json()).then((data) => {
+			fetch("/api").then((res) => res.json()).then((data: WeatherStation[]) => {
 				setData(data);
+				setSelectedNode(data[0])
 			})
 		}, 5000)
 	}, []);
 
+	const [navbarOpen, setNavbarOpen] = useState(false);
+	const [selectedItem, setSelectedItem] = useState<string>("data");
+
+	const iconSize = 55
 	return (
-		<main>
-			<div className={"p-2 flex flex-row items-center gap-x-4"}>
-				<Image
-					src={"/favicon.png"}
-					alt={"Mars"}
-					width={50}
-					height={50} />
-				<h1 className={`text-4xl font-bold`}>Mars Weather Dashboard</h1>
+		<main className={"flex flex-row"}>
+			<div id={"nav"} className={"flex flex-row gap-x-2 h-[100vh] p-2 items-start"}>
+				<div className={"flex flex-col gap-y-4"}>
+					<button onClick={() => setNavbarOpen(!navbarOpen)}>
+						<Image
+							src={"/favicon.png"}
+							alt={"Mars"}
+							className={"hover:bg-[#474747] rounded-md p-1"}
+							width={iconSize}
+							height={iconSize}/>
+					</button>
+					<NavIcon id={"reagent-bottle"} />
+					<NavIcon id={"wind"} />
+					<NavIcon id={"earthquake"} />
+				</div>
+				{
+					navbarOpen && (
+						<div className={"flex flex-col gap-y-2"}>
+							<h1 className={`text-3xl font-bold`}>MarsWeather</h1>
+							<p>test</p>
+						</div>
+					)
+				}
 			</div>
 		</main>
 	);
+}
+
+function NavIcon({ id, callback }: { id: string, callback: () => Promise<void> | undefined }) {
+
+	const iconSize = 55
+
+	return (
+		<Image
+			src={`/${id}.svg`}
+			alt={id.toUpperCase()}
+			className={"hover:bg-[#474747] rounded-md p-1"}
+			width={iconSize}
+			height={iconSize} />
+	)
+}
+
+function WindInfoPanel() {
+	return (
+		<>
+
+		</>
+	)
 }
